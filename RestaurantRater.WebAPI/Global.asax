fb81b0b40a177841dd98e194628163eb3d4d6453
Dillan Groce <groce.dillan@gmail.com>
2022-01-06 19:52:55 -0500

    Add project files.

---
+++ RestaurantRater.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestaurantRater.WebAPI.MvcApplication" Language="C#" %>
---
+++ RestaurantRater.WebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRater.WebAPI.Startup))]
+namespace RestaurantRater.WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
